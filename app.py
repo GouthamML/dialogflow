53d72eac0dc586f21a6513caf258a545ec8bdb90
Goutham ML <goutham24gml@gmail.com>
2020-04-16 01:59:54 +0530

    create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import flask
+import json
+from flask import render_template
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/test_flask', methods=['POST'])
+def test_flask():
+    req_data = request.get_json()
+    return req_data
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
